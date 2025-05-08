6a773c03cb55d6aee57af395263918afee481763
ShamimHossain1 <shamimweb78@gmail.com>
2025-05-08 18:42:24 -0400

    problem 6

--- assignment1.ts
+++ assignment1.ts
@@ -40,3 +40,23 @@ function processValue(value: string | number): number{
     return typeof value === "string" ? value.length : value * 2;
 }
 
+
+
+interface Product {
+  name: string;
+  price: number;
+}
+
+function getMostExpensiveProduct(products: Product[]): Product | null{
+    if (products.length === 0) {
+        return null;
+    }else{
+        return products.reduce((prev, curr) => prev.price > curr.price ? prev : curr);
+    }
+}
+
+
+
+
+
+
