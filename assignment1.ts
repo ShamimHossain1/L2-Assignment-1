fc61d2e4b0705476df9d8f0cd6fad8b7e5f7b5fb
ShamimHossain1 <shamimweb78@gmail.com>
2025-05-08 18:01:19 -0400

    problem 1

---
+++ assignment1.ts
@@ -0,0 +1,10 @@
+function formatString(input: string, toUpper?: boolean): string {
+    if (toUpper == true || toUpper == undefined) {
+        return input.toUpperCase();
+    } else {
+        return input.toLowerCase();
+
+    }
+}
+
+
