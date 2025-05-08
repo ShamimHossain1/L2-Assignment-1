c8dffdf5bf3650901355d820de3877db2942eaf8
ShamimHossain1 <shamimweb78@gmail.com>
2025-05-08 18:47:21 -0400

    problem 7

--- assignment1.ts
+++ assignment1.ts
@@ -57,6 +57,26 @@ function getMostExpensiveProduct(products: Product[]): Product | null{
 
 
 
+enum Day {
+  Monday,
+  Tuesday,
+  Wednesday,
+  Thursday,
+  Friday,
+  Saturday,
+  Sunday
+}
+
+function getDayType(day: Day): string{
+
+    if(day == Day.Saturday || day == Day.Sunday){
+        return "Weekend";
+    }else{
+        return "Weekday";
+    }
+}
+
+
 
 
 
