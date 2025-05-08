da3376f6cefb6d7697c21aa2d840364e5853a006
ShamimHossain1 <shamimweb78@gmail.com>
2025-05-08 18:01:30 -0400

    problem 2

--- assignment1.ts
+++ assignment1.ts
@@ -7,4 +7,9 @@ function formatString(input: string, toUpper?: boolean): string {
     }
 }
 
+function filterByRating(items: { title: string; rating: number }[]): { title: string; rating: number }[]{
+    return items.filter(item=>item.rating >= 4.0)
+}
+
+
 
